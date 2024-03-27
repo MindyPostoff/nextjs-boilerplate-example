8a9a8791b9aa304a337b4a305071b8db7d6f71fd
MindyPostoff <postoff.mindy@gmail.com>
2024-03-27 12:01:48 -0400

    Adding accordion to main page

--- app/page.tsx
+++ app/page.tsx
@@ -108,6 +108,29 @@ export default function Home() {
           </p>
         </a>
       </div>
+
+      <div className="p-5">
+        <Accordion>
+          <AccordionItem key="1" aria-label="Accordion 1" subtitle="Press to expand" title="Accordion 1">
+          {defaultContent}
+          </AccordionItem>
+          <AccordionItem
+            key="2"
+            aria-label="Accordion 2"
+            subtitle={
+              <span>
+                Press to expand <strong>key 2</strong>
+              </span>
+            }
+            title="Accordion 2"
+          >
+          {defaultContent}
+          </AccordionItem>
+          <AccordionItem key="3" aria-label="Accordion 3" subtitle="Press to expand" title="Accordion 3">
+            {defaultContent}
+          </AccordionItem>
+        </Accordion>
+      </div>
     </main>
   );
 }
